a04138f02ce21715175138f3548d42b7db2d0dbb
Wajid Usto <wajidusto@gmail.com>
2024-11-02 13:36:27 +0500

    command added

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+
+// Nav Bar
+// Nav Bar
+
 let resMenu = document.querySelector("#nav-2-menu-bar")
 let navSvg = document.querySelector("#nav-items svg")
 let cross = document.querySelector(".ri-close-line")
-let body = document.querySelector("body")
+let navBuyBtn = document.querySelector("#nav-buy-btn")
+let navBuyDiv = document.querySelector("#nav-buy")
 
 navSvg.addEventListener("click",()=>{
         resMenu.style.left = "0%"
@@ -9,5 +14,13 @@ navSvg.addEventListener("click",()=>{
 cross.addEventListener("click",()=>{
     resMenu.style.left = "-100%"
 })
+navBuyBtn.addEventListener("mouseover",()=>{
+    navBuyDiv.style.display = "block"
+})
+navBuyDiv.addEventListener("mouseleave",()=>{
+    navBuyDiv.style.display = "none"
+})
 
 
+// Nav Bar
+// Nav Bar
